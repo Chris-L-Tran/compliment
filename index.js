3254bcabe4f00c51f112ab4bff30c8724566ce84
Christopher Tran <christopher.long.tran@gmail.com>
2018-01-02 00:25:20 -0500

    created compliments variables, added test to display first compliment

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require('express')
 const app = express()
 
+var randCompliment = ''
+
+var compliments = [
+  'Your instructors love you',
+  'High five = ^5',
+  'Is it Ruby Tuesday yet?',
+  "It's almost beer o'clock",
+  'The Force is strong with you'
+]
+
 app.set('view engine', 'hbs')
 
 app.listen(5000, () => {
@@ -8,5 +18,6 @@ app.listen(5000, () => {
 })
 
 app.get('/', (req, res) => {
-  res.render('index')
+  randCompliment = compliments[0]
+  res.render('index', {randCompliment})
 })
