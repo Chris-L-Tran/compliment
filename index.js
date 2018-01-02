2b20cb273ff879dfc46a40bf6617e2a5f253cf00
Christopher Tran <christopher.long.tran@gmail.com>
2018-01-02 00:15:55 -0500

    created and tested hbs views

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
 
+app.set('view engine', 'hbs')
+
 app.listen(5000, () => {
   console.log('listening on port 5000')
+})
+
+app.get('/', (req, res) => {
+  res.render('index')
 })
